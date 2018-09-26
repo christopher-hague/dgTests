099d34940316eef5ca6ca41e19ab23c3ce1b6ed9
christopher-hague <christopher.a.hague@gmail.com>
2018-09-25 22:44:53 -0600

    fix typos

--- javascript/problems/allVowels.js
+++ javascript/problems/allVowels.js
@@ -1,14 +1,5 @@
 const allVowels = (str) => {
     // your code here
-    let vowels = "aeiou".split("")
-    let result = []
-    str.split("").forEach(char => {
-      if(vowels.includes(char.toLowerCase())) {
-          result.push(char)
-      }
-    })
-
-    return result
 }
 
 exports._test = {
